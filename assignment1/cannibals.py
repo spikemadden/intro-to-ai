417b6c4d7c79f3514972bbd4933647a2f41d2005
Spike Madden <maddens@oregonstate.edu>
2017-04-17 17:57:52 -0700

    add expand function

--- assignment1/cannibals.py
+++ assignment1/cannibals.py
@@ -27,7 +27,13 @@ def load_data(file):
 		else:
 			right_bank = list(map(int,line[0:3]))
 
-	return left_bank, right_bank
+	# check boat position and create state
+	if (right_bank[2] == 1):
+		state = State(right_bank[0], right_bank[1], left_bank[0], left_bank[1], 'right')
+	else:
+		state = State(right_bank[0], right_bank[1], left_bank[0], left_bank[1], 'left')
+
+	return state
 
 def main(args):
 	print (len(args))
@@ -37,13 +43,14 @@ def main(args):
 		print ("Usage: starting_state goal_state mode output_file")
 		return
 
-		# Save file name
+	# Save file name
 	starting_state = args[1]
 	goal_state = args[2]
 	mode = args[3]
 	output = args[4]
 
-	left, right = load_data(starting_state)
+	initial_state = load_data(starting_state)
+	goal_state = load_data(goal_state)
 
 	validModes = set(["bfs", "dfs", "iddfs", "astar"])
 	if mode not in validModes:
@@ -56,8 +63,65 @@ def main(args):
 if __name__ == "__main__":
 	main(sys.argv)
 
-def breadth_first(initial_left, initial_right, goal):
+def breadth_first(initial_state, goal_state):
+	visited = set()
 	fringe = []
-	initial = State(initial_right[0], initial_right[1], initial_left[0], initial_left[1], 'right')
-
+	# push first node into queue
+	fringe.append(initial_state)
 	while(fringe):
+		# gets the first element in the fringe
+		current_node = fringe.remove()
+		# check if current node is goal
+		if (current_node == goal):
+			return
+		if (current_node not in visited):
+			visited.add(current_node)
+			#FIX THIS BOI
+			valid_children = expand(current_node)
+
+def expand(current):
+	valid_children = []
+	if (current.boat == 'right'):
+		# move one missionary
+		child = State(current.misRight - 1, current.canRight, current.misLeft + 1, current.canLeft, 'left')
+		if (child.isValid()):
+			valid_children.append(child)
+		# move two missionaries
+		child = State(current.misRight, current.canRight - 2, current.misLeft, current.canLeft + 2, 'left')
+		if (child.isValid()):
+			valid_children.append(child)
+		# move one cannibal
+		child = State(current.misRight, current.canRight - 1, current.misLeft, current.canLeft + 1, 'left')
+		if (child.isValid()):
+			valid_children.append(child)
+		# move one cannibal and one missionary
+		child = State(current.misRight - 1, current.canRight - 1, current.misLeft + 1, current.canLeft + 1, 'left')
+		if (child.isValid()):
+			valid_children.append(child)
+		# move two cannibals
+		child = State(current.misRight, current.canRight - 2, current.misLeft, current.canLeft + 2, 'left')
+		if (child.isValid()):
+			valid_children.append(child)
+	elif (current.boat == 'left'):
+		# move one missionary
+		child = State(current.misRight + 1, current.canRight, current.misLeft - 1, current.canLeft, 'right')
+		if (child.isValid()):
+			valid_children.append(child)
+		# move two missionaries
+		child = State(current.misRight, current.canRight + 2, current.misLeft, current.canLeft - 2, 'right')
+		if (child.isValid()):
+			valid_children.append(child)
+		# move one cannibal
+		child = State(current.misRight, current.canRight + 1, current.misLeft, current.canLeft - 1, 'right')
+		if (child.isValid()):
+			valid_children.append(child)
+		# move one cannibal and one missionary
+		child = State(current.misRight + 1, current.canRight + 1, current.misLeft - 1, current.canLeft - 1, 'right')
+		if (child.isValid()):
+			valid_children.append(child)
+		# move two cannibals
+		child = State(current.misRight, current.canRight + 2, current.misLeft, current.canLeft - 2, 'right')
+		if (child.isValid()):
+			valid_children.append(child)
+
+	return valid_children
